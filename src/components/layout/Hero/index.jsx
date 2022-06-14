e3118da846d8c6bdcd52a3ad722c8495847b7a64
Joel Garay <joelgaray2020@outlook.com>
2022-06-14 16:18:27 -0500

    ADD heroImg

--- src/components/layout/Hero/index.jsx
+++ src/components/layout/Hero/index.jsx
@@ -1,5 +1,44 @@
 import styles from './hero.module.css'
+import { useNewsByCategory } from './../../../hooks/useNewsByCategory';
+import { newsCategories } from '../../../services/newsApi/getNewsCategories';
 
 export function Hero() {
-  return <div className={`wrapper ${styles.hero}`}>PORTADA</div>
+
+  const {data,loading } = useNewsByCategory({
+    category: newsCategories.technology.category,
+    pageSize: 1
+  });
+
+  if (loading)  {
+    return <div className={`wrapper ${styles.hero}`}><p>Cargando Imagen</p></div>
+  }
+
+  return (
+    <div className={`wrapper ${styles.hero}`}>
+      {
+        data?.length === 0
+        ?
+        <p>No Disponible</p>
+        :
+        data?.map((item,i) => {
+          return (
+            <div key={item.title + i} className={styles.hero__image}>
+              <img src={item.urlToImage} alt="" />
+              <div className={styles.hero__imageDescription}>
+                <a href={item.url}>
+                  <h3>{item.title}</h3>
+                </a>
+                <p>{item.description}</p>
+                <small>
+                    <span>Autor: {item.author}</span>
+                    <span>Fuente: {item.source.name}</span>
+                </small>
+              </div>
+              
+            </div>
+          )
+        })
+      }
+    </div>
+  )
 }
--- src/services/newsApi/getNewsByCategory.js
+++ src/services/newsApi/getNewsByCategory.js
@@ -2,10 +2,10 @@ import { config } from '../../config'
 
 const { API_KEY, API_URL } = config.newsApi
 
-export function getNewsByCategory({ category, pageSize = 20 }) {
+export async function getNewsByCategory({ category, pageSize = 20 }) {
   if (!category) return []
   const url = `${API_URL}/top-headlines?category=${category}&pageSize=${pageSize}&language=es&apiKey=${API_KEY}`
-  return fetch(url)
+  return await fetch(url)
     .then((res) => res.json())
     .then((res) => {
       const { articles } = res
