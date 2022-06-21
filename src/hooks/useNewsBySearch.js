cf3bac289831f884f471235fe168fe643d70aced
Joel Garay <joelgaray2020@outlook.com>
2022-06-20 20:56:47 -0500

    ADD lastSearch

--- src/hooks/useNewsBySearch.js
+++ src/hooks/useNewsBySearch.js
@@ -1,24 +1,24 @@
 import { useEffect, useState } from 'react'
 import { getNewsBySearch } from '../services/newsApi/getNewsBySearch';
 
-export function useNewsBySearch({ search, page }) {
+export function useNewsBySearch({ search, pageSize, page }) {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(null);
     const [isError, setIsError] = useState(null);
-
+    
     useEffect(() => {
         setIsLoading(true);
-        getNewsBySearch({ search, page })
-            .then((news) => {
-                setData(news);
-                setIsLoading(false);
-            })
-            .catch((error) => {
+        getNewsBySearch({ search, pageSize, page })
+        .then((news) => {
+            setData(news);
+            setIsLoading(false);
+        })
+        .catch((error) => {
                 setIsLoading(false);
                 setIsError(true);
                 throw new Error(error);
             })
-    }, [search, page])
+    }, [search, pageSize,page])
 
   return { data, isLoading, isError}
 }
--- src/services/newsApi/getNewsBySearch.js
+++ src/services/newsApi/getNewsBySearch.js
@@ -2,8 +2,8 @@ import { config } from '../../config'
 
 const { API_KEY, API_URL } = config.newsApi
 
-export async function getNewsBySearch({ search = {}, page = 1 }) {
-  const url = `${API_URL}/everything?qInTitle=${search}&page=${page}&language=es&apiKey=${API_KEY}`
+export async function getNewsBySearch({ search = {}, pageSize = 20,page = 1 }) {
+  const url = `${API_URL}/everything?qInTitle=${search}&pageSize=${pageSize}&page=${page}&language=es&apiKey=${API_KEY}`
   return await fetch(url)
     .then((res) => res.json())
     .then((res) => {
