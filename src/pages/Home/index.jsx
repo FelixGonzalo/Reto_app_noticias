c8981b76445716ffdb27aca442a2f9b44fe6d3c0
JoseMiguelVM <jverama@unsa.edu.pe>
2022-06-23 22:14:43 -0500

    FIX react-icons implementation

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -7,6 +7,10 @@ import { useNewsByCategory } from '../../hooks/useNewsByCategory'
 import { newsCategories } from '../../services/newsApi/getNewsCategories'
 import styles from './home.module.css'
 import { ReadingAssistanceMenu } from '../../components/ReadingAssistanceMenu'
+//Agregando la importacion para los iconos
+import {IoGameController} from 'react-icons/io5';
+import {GiSoccerBall, GiMoneyStack} from 'react-icons/gi';
+
 
 const Home = () => {
   const { data: heroData, loading: heroLoading } = useNewsByCategory({
@@ -58,7 +62,8 @@ const Home = () => {
       <main className={`wrapper ${styles.home__main}`}>
         <section>
           <h3 className={styles.subTitle}>
-            🌎 {newsCategories.entertainment.categoria}
+            &nbsp; <IoGameController style={{color: 'red', fontSize: '26px'}}/> &nbsp; 
+            {newsCategories.entertainment.categoria}
           </h3>
           <NewsList data={entertainmentData} isloading={entertainmentLoading} />
           <Link
@@ -70,7 +75,8 @@ const Home = () => {
         </section>
         <section>
           <h3 className={styles.subTitle}>
-            ⚽ {newsCategories.sports.categoria}
+            &nbsp; <GiSoccerBall style={{color: 'red', fontSize: '26px'}}/> &nbsp; 
+            {newsCategories.sports.categoria}
           </h3>
           <NewsList
             data={sportsData}
@@ -86,7 +92,8 @@ const Home = () => {
         </section>
         <section>
           <h3 className={styles.subTitle}>
-            🪙 {newsCategories.business.categoria}
+            &nbsp; <GiMoneyStack style={{color: 'red', fontSize: '26px'}}/> &nbsp; 
+            {newsCategories.business.categoria}
           </h3>
           <NewsList
             data={businessData}
@@ -100,9 +107,27 @@ const Home = () => {
             Ver más
           </Link>
         </section>
+
+        <aside>
+          <div class="publicidad">
+            <div class="cuadro1">
+              <div class="subcuadro1">
+                <marquee bgcolor = "red" behavior="scroll" direction="left">me pruebas</marquee>
+              </div>
+            </div>
+
+            <div class="cuadro2">
+              <div class="subcuadro2"></div>
+            </div>
+            <div class="cuadro3">
+              <div class="subcuadro3"></div>
+            </div>
+          </div>
+        </aside>
       </main>
     </main>
   )
 }
 
 export default Home
+
